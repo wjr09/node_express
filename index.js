ee91ec59c11a746f44d8b7dfc050a6f911f418b2
EA - Wellington Soares Ferreira Junior <wellington_soares@educadventista.org>
2023-10-10 14:16:35 -0300

    recebendo parametros na rota

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+const { constants } = require("buffer")
 const express = require("express")
 const path = require("path")
 const app = express()
 const caminhoBase = path.join(__dirname, "templates")
+
+app.get('/usuarios/:id', (requisicao, resposta) => {
+    const id = requisicao.params.id
+    console.log(`acessando dados do usuario: ${id}`)
+    resposta.sendFile(`${caminhoBase}/usuarios.html`)
+})
 app.get('/', (requisicao, resposta) => {
     resposta.sendFile(`${caminhoBase}/index.html`)
 
