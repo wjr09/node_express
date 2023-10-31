81413fe3e06cf4016fa9edd4996d3f15400efe4f
EA - Wellington Soares Ferreira Junior <wellington_soares@educadventista.org>
2023-10-31 14:20:24 -0300

    recebendo informacoes do formulario no corpo da requisiçao

--- index.js
+++ index.js
@@ -4,6 +4,22 @@ const path = require("path")
 const app = express()
 const caminhoBase = path.join(__dirname, "templates")
 
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json())
+
+app.post('/cadastrar/salvar', (requisicao, resposta) => {
+    const nome = requisicao.body.nome
+    const email = requisicao.body.email
+    const senha = requisicao.body.senha
+})
+
+
+app.get('/cadastrar', (requisicao, resposta) =>{
+    resposta.sendFile(`${caminhoBase}/cadastro.html`)
+})
+
 app.get('/usuarios/:id', (requisicao, resposta) => {
     const id = requisicao.params.id
     console.log(`acessando dados do usuario: ${id}`)
